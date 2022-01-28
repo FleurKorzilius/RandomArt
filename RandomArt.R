ea90440fb2a6460e220ba155c47d9e8e3fa056bf
FleurKorzilius <94118712+FleurKorzilius@users.noreply.github.com>
2022-01-28 14:50:05 +0100

    Update RandomArt.R

--- RandomArt.R
+++ RandomArt.R
@@ -4,26 +4,6 @@
 
 library(ggplot2)
 
-set.seed()
-
-x <- sample(100)
-y <- sample(100)
-df_art <- data.frame(x,y)
-
-art <- ggplot(df_art, aes(x, y)) +
-  geom_point(aes(color = y))
-art + theme(axis.title = element_blank()) +
-  theme(legend.position = "none",
-        panel.background = element_blank(),
-        axis.text = element_blank(),
-        axis.ticks.x = element_blank(),
-        axis.ticks.y = element_blank()) +
-  stat_density_2d_filled(aes(fill = ..level..), geom="polygon") +
-  scale_fill_gradient(low = palette1)
-
-colors1 <- sample(1)
-palette1 <- rainbow(colors1)
-
 make_art <- function(seed = NA) {
   if (!is.na(seed)) {
     set.seed(seed)
@@ -43,6 +23,6 @@ make_art <- function(seed = NA) {
     stat_density_2d_filled(aes(fill = ..level..), geom="polygon")
 }
 make_art()
-
+make_art(24)
 
 
